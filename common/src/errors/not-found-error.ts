b17d5c8ca21c97881e8951665a1556cbeafb0091
ducnguyen96 <levinguyen.dl@gmail.com>
2021-09-16 15:22:07 +0700

    Updates

--- common/src/errors/not-found-error.ts
+++ common/src/errors/not-found-error.ts
@@ -1,12 +1,12 @@
 import { CustomError } from "./custom-error";
 
-export class NotFoundEror extends CustomError {
+export class NotFoundError extends CustomError {
   statusCode = 404;
 
   constructor() {
     super("Not Found Error");
 
-    Object.setPrototypeOf(this, NotFoundEror.prototype);
+    Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 
   serializeErrors() {
